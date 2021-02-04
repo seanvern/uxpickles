57c509f51d5d1853982b214f56161b088599325f
seanvern <svernon@lululemon.com>
2021-02-04 10:08:35 -0800

    feat: 🎸 updated layout

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,7 +2,24 @@ import React from "react"
 import "./index.css"
 
 const Footer = () => {
-  return <div className="footer-container"></div>
+  return (
+    <>
+      <div class="custom-shape-divider-bottom-1612459485">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+            class="shape-fill"
+          ></path>
+        </svg>
+      </div>
+      <div className="footer-container"></div>
+    </>
+  )
 }
 
 export default Footer
